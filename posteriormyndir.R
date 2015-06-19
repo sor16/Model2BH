f6b631302f9d3aac84afbe169b6e9772a382606c
axelornj <aoj8@hi.is>
2015-06-19 13:09:16 +0000

    model2 works but takes 50 min

--- posteriormyndir.R
+++ posteriormyndir.R
@@ -1,14 +1,15 @@
 ##figure 3 rating curve logscale with conf interval
 
-data=data.frame(W=RC$w, Q=RC$y)
-data$l_m=l_m
+data=data.frame(W=RC$w, Q=head(RC$y,length(RC$w)))
+data$l_m=l
 seq=seq(2000,20000,5)
+quantypo1=ypo[,seq]
 quantypo2=ypo2[,seq]
 quantypo3=ypo3[,seq]
 quantypo4=ypo4[,seq]
 quantmatrix=t(cbind(quantypo1,quantypo2,quantypo3,quantypo4))
 prctile=t(apply(quantmatrix, 2, quantile, probs = c(0.025,0.5, 0.975),  na.rm = TRUE))
-data$fit=prctilep[,2]
+data$fit=prctile[,2]
 data$lower=prctile[,1]
 data$upper=prctile[,3]
 rclog=ggplot(data)+geom_line(mapping=aes(l_m,fit))+theme_bw()+geom_point(mapping=aes(l_m,Q))+geom_line(mapping=aes(l_m,lower),linetype="dashed")+
